a9a513ba8683b92c8530764644e3b19687316ea4
lokito01 <togiathuan@gmail.com>
2022-04-14 18:00:43 +0700

    Database's setup and migration

---
+++ ProjectBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectBookWeb.Models;
+
+namespace ProjectBookWeb.Data;
+
+public class ApplicationDbContext :DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+    {
+
+    }
+
+    
+    public DbSet<Category>Categories { get; set; }
+}
+
