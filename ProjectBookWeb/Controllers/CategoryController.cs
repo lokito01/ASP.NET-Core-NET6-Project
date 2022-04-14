da5ff5fb0c680fa6f574af7a2178dcdf1cb5e883
lokito01 <togiathuan@gmail.com>
2022-04-14 22:29:08 +0700

    Add Bootstrap theme and icon

---
+++ ProjectBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectBookWeb.Data;
+using ProjectBookWeb.Models;
+
+namespace ProjectBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
