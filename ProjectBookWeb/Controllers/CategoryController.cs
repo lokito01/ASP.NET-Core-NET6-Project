7b47c9a079a6ca2b37cd2c429223ae0a2f606f51
Gia Thuận <92902379+lokito01@users.noreply.github.com>
2022-04-15 15:17:49 +0700

    Add Create Category View

--- ProjectBookWeb/Controllers/CategoryController.cs
+++ ProjectBookWeb/Controllers/CategoryController.cs
@@ -18,5 +18,11 @@ namespace ProjectBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
